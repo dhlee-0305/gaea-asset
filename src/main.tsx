ea3292ceb4369a14be0aa69993f41518836a7988
이창구 <cglee@gaeasoft.co.kr>
2025-07-11 10:19:15 +0900

    mui date-picker 라이브러리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,28 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 
 import App from './App.tsx';
+
 import store from '@/store/index.tsx';
+import 'dayjs/locale/ko';
 
 // 전역 스타일 import
 import '@/styles/variables.css';
 import '@/styles/globals.css';
 
+dayjs.locale('ko');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <App />
+        </LocalizationProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>,
